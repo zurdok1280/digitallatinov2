34c838a9775cb2014b5dde25d5ac5188e86a859e
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2026-04-06 10:37:00 -0600

    Update API endpoints in LoginForm component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -32,8 +32,8 @@ export function LoginForm({ onClose }) {
     setLoading(true);
 
     try {
-      //      const response = await fetch('https://security.digital-latino.com/api/auth/login', {
-      const response = await fetch('http://localhost:8085/api/auth/login', {
+           const response = await fetch('https://security.digital-latino.com/api/auth/login', {
+    //  const response = await fetch('http://localhost:8085/api/auth/login', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -74,8 +74,8 @@ export function LoginForm({ onClose }) {
     setLoading(true);
 
     try {
-      //      const response = await fetch('https://security.digital-latino.com/api/auth/register', {
-      const response = await fetch('http://localhost:8085/api/auth/register', {
+           const response = await fetch('https://security.digital-latino.com/api/auth/register', {
+     // const response = await fetch('http://localhost:8085/api/auth/register', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
